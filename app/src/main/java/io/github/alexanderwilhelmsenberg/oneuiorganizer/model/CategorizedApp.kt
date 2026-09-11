514a54a3f4f19e0ab753ecc40d0899f3d59c8221
AlexanderWilhelmsenBerg <74223409+AlexanderWilhelmsenBerg@users.noreply.github.com>
2026-09-11 14:30:09 +0200

    Use resolved category definitions for categorized apps

--- app/src/main/java/io/github/alexanderwilhelmsenberg/oneuiorganizer/model/CategorizedApp.kt
+++ app/src/main/java/io/github/alexanderwilhelmsenberg/oneuiorganizer/model/CategorizedApp.kt
@@ -1,3 +1,7 @@
 package io.github.alexanderwilhelmsenberg.oneuiorganizer.model
 
-data class CategorizedApp(val app: InstalledApp, val category: AppCategory, val source: ClassificationSource)
+data class CategorizedApp(
+    val app: InstalledApp,
+    val category: CategoryDefinition,
+    val source: ClassificationSource
+)
